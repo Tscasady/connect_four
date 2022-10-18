aa84f0b704da102df7632ee0d49feb863eb81a64
Lacey Weaver <jlweave89@gmail.com>
2022-10-18 11:45:33 -0600

    Add outline for piece class

---
+++ lib/piece.rb
@@ -0,0 +1,7 @@
+class Piece
+  attr_reader
+  
+  def initialize
+
+  end
+end
--- spec/piece_spec.rb
+++ spec/piece_spec.rb
@@ -13,4 +13,16 @@ RSpec.describe Piece do
       expect(@piece).to be_a(Piece)
     end
   end
+
+  xdescribe '#symbols' do
+    it 'can have piece for player and computer' do
+
+    end
+  end
+
+  xdescribe '#own_position' do
+    it "can check postion based on 'x' and 'y' axis" do
+
+    end
+  end
 end
