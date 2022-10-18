bef7d60e3c04ccce0d9dd502663e33589ffe5a02
Lacey Weaver <jlweave89@gmail.com>
2022-10-18 11:34:19 -0600

    Add lib and spec folders and initialize Piece class

---
+++ spec/piece_spec.rb
@@ -0,0 +1,16 @@
+#symbol 'x' - player, 'o' - computer
+#own position 'x' and 'y' -two seperate
+
+require './lib/piece'
+
+RSpec.describe Piece do
+  before (:each) do
+    @piece = Piece.new()
+  end
+
+  describe '#initialize' do
+    it 'exists and has readable attributes' do
+      expect(@piece).to be_a(Piece)
+    end
+  end
+end
