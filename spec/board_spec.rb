ccaf04c1aed64b95564d111f49871d9427b773d4
Thomas Casady <tscasady@gmail.com>
2022-10-18 19:21:31 -0500

    test: Add tests for Board class.

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,1 +1,29 @@
 require './lib/board'
+
+RSpec.describe Board do
+  describe '#initialize' do
+    it 'exists' do
+      board = Board.new
+
+      expect(board).to be_a Board
+    end
+
+    it 'has a column index hash' do
+      board = Board.new
+
+      expect(board.col_indices).to eq {}
+    end
+
+    it 'has an array of seven empty column arrays' do
+      board = Board.new
+
+      expect(board.grid).to eq [[],[],[],[],[],[],[]] 
+    end
+
+    it 'has a game_state' do
+      board = Board.new
+
+      expect(board.game_state).to eq 'none'
+    end
+  end
+end
