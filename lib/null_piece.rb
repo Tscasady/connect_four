074b767931d111397276e2be15e653086cc40367
Thomas Casady <tscasady@gmail.com>
2022-10-19 16:42:40 -0500

    feat: Complete null_piece and tests.

--- lib/null_piece.rb
+++ lib/null_piece.rb
@@ -1,2 +1,6 @@
 class NullPiece
+  attr_reader :symbol
+  def initialize
+    @symbol = '.'
+  end
 end
--- spec/null_piece_spec.rb
+++ spec/null_piece_spec.rb
@@ -1,1 +1,15 @@
 require './lib/null_piece'
+
+RSpec.describe NullPiece do
+  describe '#initialize' do
+    it 'exists' do
+      null = NullPiece.new
+
+      expect(null).to be_a NullPiece
+    end
+
+    it "has default symbol '.'" do
+      expect(NullPiece.new.symbol).to eq '.'
+    end
+  end
+end
