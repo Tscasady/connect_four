22e860586b19aefcffd6111f4d29e9b6f16b4f0f
Thomas Casady <tscasady@gmail.com>
2022-10-21 14:47:14 -0500

    docs: Add comments to help explain recursive function.

--- lib/win_check.rb
+++ lib/win_check.rb
@@ -21,17 +21,21 @@ class WinCheck
       (-1..1).each do |y_mod|
         check_count = 0
         piece_to_test = board.columns[piece.x_pos + x_mod][piece.y_pos + y_mod]
-        check(piece, piece_to_test, x_mod, y_mod, check_count, board)
+        check(piece, piece_to_test, x_mod, y_mod, check_count)
+        #reverse check, we need to check in the opposite direction and add it to our total checks
+        check(piece, piece_to_test, x_mod, y_mod, check_count )
       end
     end
   end
 
 
-  def check(piece, piece_to_test, x_mod, y_mod, check_count, board) 
-    if board.guaranteed_symbol(piece) == board.guaranteed_symbol(piece_to_test)
+  def check(piece, piece_to_test, x_mod, y_mod, check_count) 
+    #escape condition
+    #base case: 1 or more, inputs for which the function produces a result that continues the recursion (opposite of escape condition)
+    #knowing if and when recursion is useful 
+    if @board.fetch_piece(piece).symbol == @board.fetch_piece(piece_to_test).symbol
       check_count += 1
-      puts check_count
-      next_piece = board.columns[piece.x_pos + x_mod][piece.y_pos + y_mod]
+      next_piece = @board.columns[piece.x_pos + x_mod][piece.y_pos + y_mod]
       check(piece_to_test, next_piece, x_mod, y_mod, check_count, board)
     end
     check_count      
