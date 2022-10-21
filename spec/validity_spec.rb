575d4b66dbed883da6b309eda2c97bc4396e6ea0
Lacey Weaver <jlweave89@gmail.com>
2022-10-21 16:10:17 -0600

    Add spec file and tests for validity check class

---
+++ spec/validity_spec.rb
@@ -0,0 +1,38 @@
+require './lib/board'
+require './lib/piece'
+require './lib/turn'
+require './lib/null_piece'
+require './lib/game'
+require './lib/vadility'
+
+RSpec.describe Validity do
+  describe '#initialize' do
+    it 'exists' do
+      validity = Validity.new(input)
+  
+      expect(validity).to be_a(Validity)
+    end
+  end
+
+  describe '#valid_player_input?' do
+    it 'returns a boolean for whether or not the input is a valid letter' do
+    input = 'b'
+    input2 = 'Z'
+    validity = Validity.new
+
+    expect(validity.valid_player_input?(input)).to be true
+    expect(validity.valid_player_input?(input2)).to be false
+    end
+  end
+
+  describe '#valid_col' do
+  it 'checks if column is full' do
+    #rewrite test with pieces
+    input = 'D'
+    validity = Validity.new
+
+    expect(validity.valid_col('D')).to be true
+    end
+  end
+
+end
