1c611a91adaebfb826aa9d39bf673f9ad116d0d1
Thomas Casady <tscasady@gmail.com>
2022-10-20 19:07:27 -0500

    feat: Add directions data and test to win_check init.

--- lib/win_check.rb
+++ lib/win_check.rb
@@ -1,7 +1,14 @@
 class WinCheck
-  attr_reader :piece
+  attr_reader :piece, :directions
 
   def initialize(piece)
     @piece = piece
+    #Data used to modify the [x,y] to 'look' in a direction for a win.
+    @directions = {
+      'UP'    => [0 , 1], 
+      'DOWN'  => [0, -1], 
+      'LEFT'  => [-1, 0], 
+      'RIGHT' => [1, 0]
+      }
   end
 end
--- spec/win_check_spec.rb
+++ spec/win_check_spec.rb
@@ -16,5 +16,17 @@ describe WinCheck do
 
       expect(WinCheck.new(piece).piece).to be_a Piece
     end
+
+    it 'has direction data' do
+      board = Board.new
+      piece = board.place_piece('X', 'A')
+
+      expect(WinCheck.new(piece).directions).to be == {
+        'UP'    => [0 , 1], 
+        'DOWN'  => [0, -1], 
+        'LEFT'  => [-1, 0], 
+        'RIGHT' => [1, 0]
+        }
+      end
   end
 end
