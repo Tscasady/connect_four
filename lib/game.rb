e1134f3c6d736ae1e7a83f6d1c861cd2c767d155
Lacey Weaver <jlweave89@gmail.com>
2022-10-21 15:14:15 -0600

    Refactor Game class and fixed spec file

--- lib/game.rb
+++ lib/game.rb
@@ -1,14 +1,18 @@
+require './lib/board'
+require './lib/piece'
+require './lib/turn'
+require './lib/null_piece'
+require './lib/game'
+# require './lib/win_check'
+
 class Game
-  attr_reader :game_state
+  attr_reader :game_state, :welcome_message
   def initialize
     #game_state win, player, computer, tie
     @game_state = 'none'
-  end
-
-
-  def welcome_message
-      puts "Welcome to Connect Four\n" + "Enter p to play. Enter q to quit."
-  end
+    @welcome_message = "Welcome to Connect Four\n " + "Enter p to play. Enter q to quit."
+    @board = Board.new
+    @win_check = Win_check.new(@board)
 
   def get_main_menu_input
       gets.chomp.downcase
@@ -20,6 +24,7 @@ class Game
         #call play method
       elsif letter == 'q'
         puts 'You chose to quit the game, good-bye!'
+        exit!
         #abort or exit file
       else
         puts 'Invaild entry, please try again. Enter p to play. Enter q to quit.'
@@ -27,23 +32,26 @@ class Game
       end
   end
 
-
-  def test_menu
-    welcome_message
-    do_menu_command(get_main_menu_input)
-  end
-  # def play
+  # def test_menu
   #   welcome_message
+  #   do_menu_command(get_main_menu_input)
   # end
 
+  def play
+    puts @welcome_message
+    do_menu_command(get_main_menu_input)
+    end_game_message
+  end
+
   def end_game_message
-    if game_state == player
-      puts "Congratulations #{human_name}, you've won!"
-    elsif game_state == computer
+    if win_check.game_state == 'player'
+      puts "Congratulations X, you've won!"
+    elsif win_check.game_state == 'computer'
       puts "Sorry the comupter has won."
     else 
       puts "It's a tie, no one wins."
     end
-    do_menu_command
+    #create new game object
   end
+
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,12 +3,18 @@ require './lib/piece'
 require './lib/turn'
 require './lib/null_piece'
 require './lib/game'
+# require './lib/win_check'
 
 RSpec.describe Game do 
   describe '#initialize' do
     it 'exists' do
       game = Game.new
       expect(game).to be_a(Game)
+    end
+
+    it 'returns welcome string' do
+      game = Game.new
+      expect(game.welcome_message).to eq("Welcome to Connect Four\n Enter p to play. Enter q to quit.")
     end
 
     it 'has game_state with a default value of none' do
@@ -23,33 +29,28 @@ RSpec.describe Game do
     end
   end
 
-  describe '#welcome_message' do
-    it 'returns welcome string' do
-      game = Game.new
-      expect(game.welcome_message).to eq("Welcome to Connect Four\n Enter p to play. Enter q to quit.")
-    end
+#     xit 'can quit' do
+#       game = Game.new 
+#       expect(game.can_quit?).to eq(true)
+#     end
+# #invaild input check as well
 
-    xit 'can quit' do
-      game = Game.new 
-      expect(game.can_quit?).to eq(true)
-    end
-#invaild input check as well
+#     it 'can play' do
+#       game = Game.new
+#       expect(game.can_play).to eq(true)
+#     end
+#   end
 
-    it 'can play' do
-      game = Game.new
-      expect(game.can_play?).to eq(true)
-    end
-  end
-
-  describe '#play' do #from the board class
+  xdescribe '#play' do #from the board class
     it 'will change the game state when it ends' do
       expect(game.game_state).to !=('none')
     end
   end
 
-  describe '#end_game' do
+  describe '#end_game_message' do
     it "will check game state to return a message" do
-
+      game = Game.new
+      expect(game.).to eq()
     end
   end
 
