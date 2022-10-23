8582d00f67b4a97edc46262d0498cbb2c780788d
Thomas Casady <tscasady@gmail.com>
2022-10-23 16:27:22 -0500

    Remove redundant validity checks.

--- lib/board.rb
+++ lib/board.rb
@@ -19,14 +19,6 @@ class Board
     @game_state = 'none' #consider relocating depending on win_check and game class logic
   end
 
-  def valid_player_input?(player_input)
-    @col_indices.keys.include?(player_input.upcase)
-  end
-
-  def valid_col(player_input)
-    @columns[@col_indices[player_input]].length < 7
-  end
-
   def full_board?
     @columns.all? {|column| column.length == 7}
   end
