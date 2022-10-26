247ad9bed98fe2d5f32ae256e939fe2082f76c5c
Thomas Casady <tscasady@gmail.com>
2022-10-26 11:57:31 -0500

    test: Update turn tests to accept a player object instead of a symbol.

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -3,12 +3,14 @@ require './lib/turn'
 RSpec.describe Turn do
   describe '#initialize' do
     it 'exists' do
-      turn = Turn.new('X')
+      player = Player.new('player', 'X', 'Fred')
+      turn = Turn.new(player)
       expect(turn).to be_a(Turn)
     end
 
     it 'has a symbol' do
-      turn = Turn.new('X')
+      player = Player.new('player', 'X', 'Fred')
+      turn = Turn.new(player)
       expect(turn.symbol).to eq('X')
     end
   end
@@ -16,14 +18,17 @@ RSpec.describe Turn do
   describe '#get_human_input' do
     #just check that it can be called, not the input due to it being human choice
     it 'responds to method call' do
-      turn = Turn.new('X')
+      player = Player.new('player', 'X', 'Fred')
+      turn = Turn.new(player)
       expect(turn.respond_to?(:get_human_input))
     end
   end
 
   describe '#get_computer_input' do
     it 'returns a random letter between A..G' do
-      turn = Turn.new('O')
+      player = Player.new('player', 'X', 'Fred')
+      turn = Turn.new(player)
+
       computer_choice = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
       expect(computer_choice.include?(turn.get_computer_input)).to be(true)
       expect(computer_choice.include?(turn.get_computer_input)).to be(true)
