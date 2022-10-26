c7c79549c63745a3bb74820bdbcf0952ae61c28c
Thomas Casady <tscasady@gmail.com>
2022-10-26 11:58:54 -0500

    test: Update validity tests to give board arguments for size.

--- spec/validity_spec.rb
+++ spec/validity_spec.rb
@@ -8,7 +8,7 @@ require './lib/validity'
 RSpec.describe Validity do
   describe '#initialize' do
     it 'exists' do
-      board = Board.new
+      board = Board.new(7, 6)
       validity = Validity.new(board)
   
       expect(validity).to be_a(Validity)
@@ -19,7 +19,7 @@ RSpec.describe Validity do
     it 'returns a boolean for whether or not the input is a valid letter' do
     input = 'b'
     input2 = 'Z'
-    board = Board.new
+    board = Board.new(7, 6)
     validity = Validity.new(board)
     expect(validity.valid_player_input?(input)).to be true
     expect(validity.valid_player_input?(input2)).to be false
@@ -29,7 +29,7 @@ RSpec.describe Validity do
   describe '#valid_col?' do
   it 'checks if column is full' do
     #rewrite test with pieces
-    board = Board.new
+    board = Board.new(7, 6)
     board.place_piece('X', 'D')
     board.place_piece('X', 'D')
     board.place_piece('X', 'D')
@@ -48,7 +48,7 @@ RSpec.describe Validity do
 
   describe '#validity_checks' do
     it 'can check valid player input and valid column' do
-      board = Board.new
+      board = Board.new(7, 6)
       board.place_piece('X', 'D')
       board.place_piece('X', 'D')
       board.place_piece('X', 'D')
