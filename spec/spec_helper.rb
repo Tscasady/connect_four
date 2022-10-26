11b9a3d8cfaeffdaf14b2878616536e5d798b16d
Lacey Weaver <jlweave89@gmail.com>
2022-10-26 15:10:38 -0600

    Add simplcov

---
+++ spec/spec_helper.rb
@@ -0,0 +1,12 @@
+require 'simplecov'
+SimpleCov.start
+
+require_relative 'board_spec'
+require_relative 'game_spec'
+require_relative 'menu_spec'
+require_relative 'null_piece_spec'
+require_relative 'piece_spec'
+require_relative 'player_spec'
+require_relative 'turn_spec'
+require_relative 'validity_spec'
+require_relative 'win_check_spec'
